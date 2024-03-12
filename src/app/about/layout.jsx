c05da516966578b5013a1f93d2f97b311484f1ed
shashwat pal <shashwatpal1021@gmail.com>
2024-03-12 09:52:44 +0530

    1. page and layout

---
+++ src/app/about/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutLayout = ({children}) => {
+    return (
+      <section>
+            <h1>AboutLayout</h1>
+            <div className="mt-6">{children}</div>
+      </section>
+  )
+}
+
+export default AboutLayout
---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section className="py-24 px-24">
+      <div className="container">
+        <h1 className="text-3xl font-bold">
+          Next-Js Starting
+        </h1>
+      </div>
+    </section>
+  );
+}
